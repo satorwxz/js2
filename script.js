ffb54118e65f1d2cf049317d03649720f165e122
satorwxz <bekakasymbekov23@gmail.com>
2024-07-03 22:53:07 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,96 @@
+//Задание 1
+
+//let a='hello'
+//let b='Ivan'
+//let c=a.slice(0,1)
+//let d=c.toUpperCase()
+//let f=b.slice(0,1)
+//let g=f.toLowerCase()Case()
+//console.log(d + a.slice(1,5)  +' ' + g + b.slice(1,4))
+
+//Задание 2 записать длину в переменную b
+
+//let a='lorem ipsum dolor amet'
+//let b=a.length
+//console.log(b)
+
+//Задание 3  вернуть sit lorem ipsum dolor amet
+//let a='lorem ipsum dolor sit amet'
+//let b=a.slice(18,21)
+//let c=a.slice(0,5)
+//let d=a.slice(6,17)
+//let f=a.slice(12)
+//console.log(b +' ' + c + ' ' + d + ' ' + f)
+
+//Задание 4 вернуть CHILDREn
+
+//let str='children'
+//let str2=str.slice(0,7)
+//let str3=str2.toUpperCase()
+//console.log(str3 +str.slice(7))
+
+
+//Задание 5 вернуть bsso,
+
+//let a='boss'
+//let b=a.slice(0,1)
+//let c=a.slice(2,4)
+//console.log(b + c + a.slice(1,2))
+
+//Задание 6 вернуть index d
+
+//let a='sunday'
+//console.log(a[3] )
+
+
+//Задание 7 вернуть hekso
+//let a='hello'
+//let b=a.slice(0,2)
+//let c=a[a.length - 1]
+//console.log(b + 'ks' + c )
+
+
+//Задание 8 вернуть new us
+
+//let a = 'user'
+//let b = 'new'
+//console.log(b +" " + a.slice(0,2))
+
+
+//Задание 9 вернуть daydaymon
+
+//let a = 'monday'
+//let b = 'day'
+//console.log(b + b + a.slice(0,3))
+
+
+//Задание 10 вернуть 'hElLo'
+
+//let a = 'HELLO'
+//let b=a.slice(0,1)
+//let c=b.toLowerCase()
+//let d=a.slice(2,3)
+//let f=a.slice(3,4)
+//let g=a.slice(4,5)
+//console.log(c + a.slice(1,2) + d.toLowerCase() + f + g.toLowerCase())
+
+
+//Задание 11 вернуть '450'
+
+//let a = 5
+// b = '400'
+//let str=b.slice(0,1)
+//let str2=b.slice(1,2)
+//console.log(str + a + str2)
+
+
+
+//Задание 12 вернуть manak
+
+//let a = 'monkey'
+//let b = 'banana'
+//let str=a.slice(0,1)
+//let str2=b.slice(1,2)
+//let str3=b.slice(2,3)
+//let str4=a.slice(3,4)
+//console.log(str + str2 + str3 + str2 + str4)
